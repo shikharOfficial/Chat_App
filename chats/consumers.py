45b3c398279540a8039148a2ba5d4508c483bb80
shikharOfficial <shikhar.rm@metapercept.com>
2024-04-26 19:06:13 +0530

    Socket Fixes

--- chats/consumers.py
+++ chats/consumers.py
@@ -1,43 +1,89 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-
+from .models import Chats
+from datetime import datetime
+import jwt
+from django.conf import settings
+from .models import Chats
+from django.http import QueryDict
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.room_name = 'public_room'
-        self.room_group_name = self.room_name
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
-        self.accept()
+        print("CONNECTING....")
+
+        query_string = self.scope['query_string'].decode("utf8")
+        query_dict = QueryDict(query_string)
+        token = query_dict.get('token')
+        recipient_id = int(query_dict.get('recipient'))
+        user = self.authenticate_token(token)
+
+        if user:
+            self.scope["user"] = user
+            self.room_name = self.generate_room_name(user.id, recipient_id)
+            async_to_sync(self.channel_layer.group_add)(self.room_name, self.channel_name)
+            self.accept()
+        else:
+            self.close()
 
     def disconnect(self, code):
         # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
+            self.room_name, self.channel_name
         )
 
     def receive(self, text_data):
         print("Received Called")
-        json_text = json.loads(text_data)
-        print(json_text)
-        message = json_text["message"]
+
+        data = json.loads(text_data)
+        message = data['message']
+        sender_id = self.scope["user"].id
+        recipient_id = data['recipientId']
+
+        new_message = Chats.objects.create(
+            sender_id=sender_id,
+            recipient_id=recipient_id,
+            message=message,
+            sent_at=datetime.now()
+        )
         
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, 
+            self.room_name, 
             {
                 "type": "chat_message", 
-                "message": message
+                "message": message,
+                "sender": sender_id,
+                "recipient": recipient_id
             }
         )
     
     def chat_message(self, event):
         print("Chat Message Called")
         message = event['message']
-        print(event)
+        sender_id = event['sender']
+        recipient_id = event['recipient']
         
         # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        self.send(text_data=json.dumps({"message": message, "sender_id": sender_id, "recipient_id": recipient_id  }))
+
+
+    def authenticate_token(self, token):
+        try:
+            decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+            user_id = decoded_token['user_id']
+            return self.get_user(user_id)
+        except jwt.ExpiredSignatureError:
+            return None
+        except jwt.InvalidTokenError:
+            return None
+        
+    def get_user(self, userId):
+        try:
+            return Chats.objects.get(id=userId)
+        except:
+            return None 
+        
+    def generate_room_name(self, user1, user2):
+        sorted_users = sorted([user1, user2])
+        return f"chat_{sorted_users[0]}_{sorted_users[1]}"
--- chats/views.py
+++ chats/views.py
@@ -3,12 +3,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from .models import Chats
-from .serializers import ChatSerializer
 from django.forms.models import model_to_dict
 from django.shortcuts import render
 from django.db.models import Q
-
+from django.http import JsonResponse
+from .serializers import ChatSerializer
+from user.serializers import UserSerializer
+from .models import Chats
+from user.models import TblUser
 
 class ChatsView(APIView):
     authentication_classes = [JWTAuthentication]
@@ -19,7 +21,6 @@ class ChatsView(APIView):
         data = None
         message = ''
         response_status = status.HTTP_500_INTERNAL_SERVER_ERROR
-        user_id = None
 
         try:
             receiver = request.GET.get('receiver')
@@ -40,7 +41,7 @@ class ChatsView(APIView):
     def post(self, request):
         data = None
         message = ''
-        resposne_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+        response_status = 500
         success = False
 
         try:
@@ -48,7 +49,7 @@ class ChatsView(APIView):
             recipient_id = request.data.get("recipient")
 
             if sender_id == recipient_id:
-                return Response({"error": "Sender cannot be the same as recipient."}, status=status.HTTP_400_BAD_REQUEST)
+                return JsonResponse({"error": "Sender cannot be the same as recipient."}, status=400)
 
             request.data['sender'] = sender_id
 
@@ -59,15 +60,16 @@ class ChatsView(APIView):
                 data = model_to_dict(chat_instance)
                 success = True
                 message = "Message Sent Successfully"
-                resposne_status = status.HTTP_201_CREATED
+                response_status = 201
+
             else:
                 message = chat_serializer.errors
-                resposne_status = status.HTTP_400_BAD_REQUEST
+                response_status = 400
                 
         except Exception as e: 
             message = str(e)
-    
-        return Response({ "success": success, "message": message, "data": data}, status=resposne_status)
+        
+        return JsonResponse({"success": success, "message": message, "data": data}, status=response_status)
     
 
 class UsersFromChats(APIView):
@@ -83,16 +85,29 @@ class UsersFromChats(APIView):
         try:
             user_id = request.user.id
 
-            recipient_info = {
+            query = {
                 recipient['recipient__id']: {
                     "id": recipient['recipient__id'],
                     "full_name": recipient['recipient__first_name'] + ' ' + recipient['recipient__last_name'],
                     "email": recipient['recipient__email']
                 } 
                 for recipient in Chats.objects.filter(sender_id=user_id).values("recipient__id", "recipient__first_name", "recipient__last_name", "recipient__email").distinct()
-            }.values()
+            }
 
-            data = recipient_info
+            users_already_messaged = query.values()
+
+            users_already_messaged_ids = query.keys()
+
+            new_users = TblUser.objects.exclude(id=user_id).exclude(id__in=users_already_messaged_ids) 
+
+            serialized_new_users = UserSerializer(instance=new_users, many = True)
+
+            formatted_serialized_new_users = [{ "id": userDetail["id"], "full_name": userDetail["first_name"] + userDetail["last_name"], "email": userDetail["email"] } for userDetail in serialized_new_users.data]
+            
+            data = {
+                "users_already_messaged": users_already_messaged,
+                "new_users": formatted_serialized_new_users
+            }
             success = True
             message = "Users fetched Successfully"
             resposne_status = status.HTTP_200_OK
